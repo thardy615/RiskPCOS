8368627991e7b45a3000fc20695b57a87380a5fd
thardy615 <51917694+thardy615@users.noreply.github.com>
2024-12-08 18:41:41 -0500

    Update midterm_Streamlit.py

--- midterm_Streamlit.py
+++ midterm_Streamlit.py
@@ -262,7 +262,7 @@ if page == "Home":
     """)
     st.markdown("""<p style="font-size:18px;"> According to the World Health Organization(WHO), it is estimated that this condition affects 8-13% of women among reproductive age; however, 70% of cases go undiagnosed. Given the (lack of) care for women's reproductive health, it is very common for it to take years to diagnose women who do have it.</p>""", unsafe_allow_html=True)
     st.write("Source: [World Health Organization](https://www.who.int/news-room/fact-sheets/detail/polycystic-ovary-syndrome)")
-    st.write(""" ####This app aims to predict PCOS diagnosis among fertile women and compare fertility measures (AMH) among infertile and fertile women with/without PCOS.""")
+    st.write(""" #### This app aims to predict PCOS diagnosis among fertile women and compare fertility measures (AMH) among infertile and fertile women with/without PCOS.""")
     # Source Information
     
     st.markdown(""" <div style="color: black;"> Please venture through side bar options to learn more about the data used to assess PCOS risk, Initial Data Analysis, Exploratory Data Analysis, and the interactive Nomogram </div>""", unsafe_allow_html=True)
